92f147b9a3ae85168346aaa9e84d7d243f6a11d5
Ju <cju33@isye-hps0401.isye.gatech.edu>
2023-01-27 18:29:32 -0500

    script used for cluster 34968

--- src/script_4.py
+++ src/script_4.py
@@ -2,10 +2,10 @@ import os
 
 # (0,1,2,3,4) EDDP, EDDPL, EDDPUL, SDDP, SEDDP 
 modes = [1,4]
-sce_seeds = [0]
+sce_seeds = range(10)
 sel_seeds = range(1)
 
-niters = 500
+niters = 3000
 N = 10
 debug = False
 
