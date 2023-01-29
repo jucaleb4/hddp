65abc3d1c5ace9ef2ab65a33491f482a6f7ffd1b
Ju <cju33@isye-hps0401.isye.gatech.edu>
2023-01-28 23:10:41 -0500

    new scripts

---
+++ src/script_8.py
@@ -0,0 +1,22 @@
+import os
+
+# (0,1,2,3,4) EDDP, EDDPL, EDDPUL, SDDP, SEDDP 
+modes = [0,1,4]
+sce_seeds = range(10)
+sel_seeds = range(1)
+
+niters = 3000
+N = 10
+debug = False
+
+ct = 600
+
+for sce_seed in sce_seeds:
+    for sel_seed in sel_seeds:
+        for mode in modes:
+            print("ct={} mode={} scen_seed={} sel_seed={} N={} niter={}".format(ct, mode, sce_seed, sel_seed, N, niters))
+            if debug:
+                os.system("python main.py --mode {} --niters {} --scen_seed {} --sel_seed {} --N {}".format(mode, niters, sce_seed, sel_seed, N))
+            else:
+                os.system("python main.py --mode {} --niters {} --scen_seed {} --sel_seed {} --N {} >> output/run_{}.txt".format(mode, niters, sce_seed, sel_seed, N, ct))
+            ct += 1
